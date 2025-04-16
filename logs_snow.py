93e44b646748d44bcd45017b1de7aa0501a1c39b
Grzegorz Martyniuk <ghosti@o2.pl>
2025-04-16 09:25:35 +0200

    Add second db inserting function. Finish filtering notifications.log

--- logs_snow.py
+++ logs_snow.py
@@ -2,6 +2,7 @@
 
 import os
 import snowflake.connector as sc
+import re
 
 # Variables for Snowflake connection
 private_key_file='/home/gradmin/.ssh/snowflake_rsa_key.p8'
@@ -12,9 +13,11 @@ database='TEAM_INC_DEV'
 schema='PUBLIC'
 
 naemon_arr=[]
+notifications_arr=[]
 
-# Connect to DB
-def db_connection(private_key_file,dataset):
+# Connect to DB and INSERT INTO naemon_log table 2D array data converted from naemon.log file
+#
+def naemon_insert(private_key_file,dataset):
 
     print('Hello. Starting to connect to the snowflake...')
     try:
@@ -38,12 +41,37 @@ def db_connection(private_key_file,dataset):
     finally:
         con.close()
 
+# Connect to DB and INSERT INTO notifications_log table 2D array data converted from naemon.log file
+#
+def notification_insert(private_key_file,dataset):
+
+    print('Hello. Starting to connect to the snowflake...')
+    try:
+        conn_params = {
+            'account' : 'BL35257-VERISURE',
+            'user' : 'grzegorz.martyniuk@verisure.com',
+            'private_key_file' : private_key_file,
+            'warehouse' : 'TEAM_INC_WH' ,
+            'database' : 'TEAM_INC_DEV',
+            'schema' : 'PUBLIC'
+        }
+        con = sc.connect(**conn_params)
+        cs = con.cursor()
+        for row in range(len(dataset)):
+            command=("INSERT INTO NOTIFICATIONS_LOG  (date,time, data) VALUES ('{0}','{1}','{2}');".format(dataset[row][0],dataset[row][1],dataset[row][2]))
+            cs.execute(command)
+            print(row)
+        #result = select1.fetchall()
+    except BaseException as e:
+        print ("There's been a problem with DB connection with following system message: \n", e)
+    finally:
+        con.close()
 
 # Read and format naemon.log file
 def naemon_log():
 
-    nlog=open("naemon.log","r")
-    for row in nlog:
+    naelog=open("naemon.log","r")
+    for row in naelog:
         epoch = row.split("]")[0].replace("[","")
         status = row.split(":")[0].split(" ")[1:]
         data_line=row.split(":")[1:]
@@ -97,15 +125,18 @@ def naemon_log():
     return naemon_arr
 
 # Read and format notifications.log file
-#def notifications_log():
-
-
-
-# Read and format thruk.log file
-# def thruk_log():
-
+def notifications_log():
+    notlog=open('notifications.log','r')
+    for row in notlog:
+        date = row.split(" ")[0]
+        time = row.split(" ")[1]
+        data = row.split(" ")[2:]
+        data= " ".join(data)
+        notifications_arr.append([date,time,data])
+    print(notifications_arr)
 
 if __name__ == "__main__":
-    naemon_log()
-    db_connection(private_key_file,naemon_arr)
-
+    #naemon_log()
+    #db_connection(private_key_file,naemon_arr)
+    notifications_log()
+    notification_insert(private_key_file,notifications_arr)

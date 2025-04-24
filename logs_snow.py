66aede819d53e8edcd1cce9a6938fb7e2219fb2f
Grzegorz Martyniuk <ghosti@o2.pl>
2025-04-24 11:20:48 +0200

    Add site dependancy to the reading file reading and db intert function

--- logs_snow.py
+++ logs_snow.py
@@ -1,76 +1,54 @@
 #!/usr/bin/python3
 
 import os
+import socket
 import snowflake.connector as sc
 import re
 
 # Variables for Snowflake connection
+#
+nldc1_naemon_log_file = "nldc1_naemon.log"
+segum_naemon_log_file = "segum_naemon.log"
+nldc1_notifications_log_file = "nldc1_notifications.log"
+segum_notifications_log_file = "segum_notifications.log"
 private_key_file='/home/gradmin/.ssh/snowflake_rsa_key.p8'
-account='BL35257-VERISURE'
-user='grzegorz.martyniuk@verisure.com'
-warehouse='TEAM_INC_WH'
-database='TEAM_INC_DEV'
-schema='PUBLIC'
-
+conn_params = {
+    'account' : 'BL35257-VERISURE',
+    'user' : 'grzegorz.martyniuk@verisure.com',
+    'private_key_file' : private_key_file,
+    'warehouse' : 'TEAM_INC_WH' ,
+    'database' : 'TEAM_INC_DEV',
+    'schema' : 'PUBLIC'
+}
 naemon_arr=[]
 notifications_arr=[]
 
-# Connect to DB and INSERT INTO naemon_log table 2D array data converted from naemon.log file
-#
-def naemon_insert(private_key_file,dataset):
-
-    print('Hello. Starting to connect to the snowflake...')
-    try:
-        conn_params = {
-            'account' : 'BL35257-VERISURE',
-            'user' : 'grzegorz.martyniuk@verisure.com',
-            'private_key_file' : private_key_file,
-            'warehouse' : 'TEAM_INC_WH' ,
-            'database' : 'TEAM_INC_DEV',
-            'schema' : 'PUBLIC'
-        }
-        con = sc.connect(**conn_params)
-        cs = con.cursor()
-        for row in range(len(dataset)):
-            command=("INSERT INTO NAEMON_LOG (epoch_time,check_type, service, host, data) VALUES ('{0}','{1}','{2}','{3}', '{4}');".format(dataset[row][0],dataset[row][1],dataset[row][2],dataset[row][3],dataset[row][4]))
-            cs.execute(command)
-            print(row)
-        #result = select1.fetchall()
-    except BaseException as e:
-        print ("There's been a problem with DB connection with following system message: \n", e)
-    finally:
-        con.close()
-
 # Connect to DB and INSERT INTO notifications_log table 2D array data converted from naemon.log file
 #
-def notification_insert(private_key_file,dataset):
-
+def db_insert(conn_params,private_key_file,site,dataset):
     print('Hello. Starting to connect to the snowflake...')
     try:
-        conn_params = {
-            'account' : 'BL35257-VERISURE',
-            'user' : 'grzegorz.martyniuk@verisure.com',
-            'private_key_file' : private_key_file,
-            'warehouse' : 'TEAM_INC_WH' ,
-            'database' : 'TEAM_INC_DEV',
-            'schema' : 'PUBLIC'
-        }
         con = sc.connect(**conn_params)
         cs = con.cursor()
-        for row in range(len(dataset)):
-            command=("INSERT INTO NOTIFICATIONS_LOG  (date,time, data) VALUES ('{0}','{1}','{2}');".format(dataset[row][0],dataset[row][1],dataset[row][2]))
-            cs.execute(command)
-            print(row)
-        #result = select1.fetchall()
+        if len(dataset) < 10000 :
+            for row in range(len(dataset)):
+                command1=("INSERT INTO {3}_notifications_log  (date,time, data) VALUES ('{0}','{1}','{2}');".format(dataset[row][0],dataset[row][1],dataset[row][2],site))
+                cs.execute(command1)
+                print(row)
+        else:
+            for row in range(len(dataset)):
+                command1=("INSERT INTO {5}_naemon_log (epoch_time,check_type, service, host, data) VALUES ('{0}','{1}','{2}','{3}','{4}');".format(dataset[row][0],dataset[row][1],dataset[row][2],dataset[row][3],dataset[row][4],site))
+                cs.execute(command1)
+                print(row)
     except BaseException as e:
         print ("There's been a problem with DB connection with following system message: \n", e)
     finally:
         con.close()
 
 # Read and format naemon.log file
-def naemon_log():
-
-    naelog=open("naemon.log","r")
+def naemon_log(logfile):
+    del naemon_arr[:]
+    naelog=open(logfile,'r')
     for row in naelog:
         epoch = row.split("]")[0].replace("[","")
         status = row.split(":")[0].split(" ")[1:]
@@ -125,18 +103,29 @@ def naemon_log():
     return naemon_arr
 
 # Read and format notifications.log file
-def notifications_log():
-    notlog=open('notifications.log','r')
+def notifications_log(logfile):
+    del notifications_arr[:]
+    notlog=open(logfile,'r')
     for row in notlog:
         date = row.split(" ")[0]
         time = row.split(" ")[1]
         data = row.split(" ")[2:]
         data= " ".join(data)
         notifications_arr.append([date,time,data])
-    print(notifications_arr)
+    return notifications_arr
 
 if __name__ == "__main__":
-    #naemon_log()
-    #db_connection(private_key_file,naemon_arr)
-    notifications_log()
-    notification_insert(private_key_file,notifications_arr)
+    site=socket.gethostname().split("-")[0]
+    print (site)
+    site="nldc1"
+    naemon_log(nldc1_naemon_log_file)
+    notifications_log(nldc1_notifications_log_file)
+    print(site,len(naemon_arr),len(notifications_arr))
+    #db_insert(conn_params,private_key_file,site,notifications_arr)
+    site="segum"
+    naemon_log(segum_naemon_log_file)
+    notifications_log(segum_notifications_log_file)
+    print(site,len(naemon_arr),len(notifications_arr))
+    #db_insert(conn_params,private_key_file,site,notifications_arr)
+
+    #db_insert(conn_params,private_key_file,site,naemon_arr)
